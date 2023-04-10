Fix hessian implementations

_test/test_state_lifter.py
@@ -8,15 +8,15 @@ from lifters.stereo1d_lifter import Stereo1DLifter
 from lifters.stereo2d_lifter import Stereo2DLifter
 from lifters.stereo3d_lifter import Stereo3DLifter
 
-d = 2
-n_landmarks = 4
-n_poses = 1
+d = 3
+n_landmarks = 5
+n_poses = 5
 Lifters = {
     # Poly4Lifter: dict(),
     # Poly6Lifter: dict(),
     RangeOnlySLAM1Lifter: dict(n_positions=n_poses, n_landmarks=n_landmarks, d=d),
-    RangeOnlySLAM2Lifter: dict(n_positions=n_poses, n_landmarks=n_landmarks, d=d),
-    RangeOnlyLocLifter: dict(n_positions=n_poses, n_landmarks=n_landmarks, d=d),
+    # RangeOnlySLAM2Lifter: dict(n_positions=n_poses, n_landmarks=n_landmarks, d=d),
+    # RangeOnlyLocLifter: dict(n_positions=n_poses, n_landmarks=n_landmarks, d=d),
     # Stereo1DLifter: dict(n_landmarks),
     # Stereo2DLifter: dict(n_landmarks),
     # Stereo3DLifter: dict(n_landmarks),
@@ -24,7 +24,7 @@ Lifters = {
 # Below, we always reset seeds to make sure tests are reproducible.
 all_lifters = []
 for Lifter, kwargs in Lifters.items():
-    np.random.seed(0)
+    np.random.seed(1)
     all_lifters.append(Lifter(**kwargs))
 
 
@@ -34,31 +34,33 @@ def test_hess_finite_diff():
         lifter.sample_feasible()
 
         errors = []
-        eps_list = np.logspace(-10, -5, 11)
+        eps_list = np.logspace(-10, -5, 3)
         for eps in eps_list:
-            Q, y = lifter.get_Q(noise=1)
+            Q, y = lifter.get_Q(noise=1e-2)
             theta = lifter.get_vec_around_gt(delta=0).flatten("C")
 
             try:
                 grad = lifter.get_grad(theta, y)
                 hess = lifter.get_hess(theta, y)
             except Exception as e:
-                # print(e)
-                # print("get_hess not implemented?")
+                print(e)
+                print("get_hess not implemented?")
                 continue
 
             n = len(theta)
             I = np.eye(n) * eps
 
             max_err = -np.inf
+            errors_mat = np.full((n, n), -np.inf)
             for i in range(n):
                 theta_delta = theta + I[i]
                 grad_delta = lifter.get_grad(theta_delta, y)
 
                 hess_est = (grad_delta - grad) / eps
 
-                err = np.max(np.abs(hess_est - hess[i, :]))
-                max_err = max(err, max_err)
+                abs_error = np.abs(hess_est - hess[i, :])
+                errors_mat[i, :] = np.maximum(abs_error, errors_mat[i, :])
+                max_err = max(np.max(abs_error), max_err)
             errors.append(max_err)
 
         try:
@@ -91,14 +93,15 @@ def test_grad_finite_diff():
             try:
                 grad = lifter.get_grad(theta, y)
             except Exception as e:
-                # print(e)
-                # print("grad not implemented?")
+                print(e)
+                print("grad not implemented?")
                 continue
 
             n = len(theta)
             I = np.eye(n) * eps
 
             max_err = -np.inf
+            errors_mat = np.full(n, -np.inf)
             for i in range(n):
                 theta_delta = theta + I[i]
                 cost_delta = lifter.get_cost(theta_delta, y)
@@ -106,8 +109,8 @@ def test_grad_finite_diff():
                 grad_est = (cost_delta - cost) / eps
 
                 err = abs(grad_est - grad[i])
+                errors_mat[i] = max(errors_mat[i], err)
                 max_err = max(err, max_err)
-                break
 
             errors.append(max_err)
 
@@ -191,6 +194,9 @@ def test_cost(noise=0.0):
 
         x = lifter.get_x(theta)
         costQ = abs(x.T @ Q @ x)
+
+        # TODO(FD) figure out why the tolerance is so bad
+        # for Stereo3D problem.
         assert abs(cost - costQ) < 1e-7, (cost, costQ)
 
         if noise == 0 and not isinstance(lifter, PolyLifter):
@@ -246,14 +252,19 @@ def test_solvers(n_seeds=1, noise=0.0):
                 assert cost_hat <= cost_0
 
                 # TODO(FD) this doesn't pass, looks like the problem is actually not well conditioned!
-                # Need to implement and investigate Hessian!
                 try:
                     np.testing.assert_allclose(theta_hat, theta_gt, atol=1e-5)
                 except AssertionError as e:
                     print(
                         f"Found solution for {lifter} is not ground truth in zero-noise! is the problem well-conditioned?"
                     )
-                    print(e)
+                    mineig_hess_hat = np.linalg.eigvalsh(lifter.get_hess(theta_hat, y))[
+                        0
+                    ]
+                    mineig_hess_gt = np.linalg.eigvalsh(lifter.get_hess(theta_gt, y))[0]
+                    print(
+                        f"minimum eigenvalue at gt: {mineig_hess_gt:.1e} and at estimate: {mineig_hess_hat:.1e}"
+                    )
             else:
                 # test that "we made progress"
                 progress = np.linalg.norm(theta_0 - theta_hat)
@@ -287,26 +298,118 @@ def test_constraints():
         test_with_tol(A_known, tol=1e-10)
 
 
+def test_gauge():
+    import itertools
+
+    # TODO(FD): understand why 3D needs 5 landmarks and positions
+    params = [
+        dict(n_landmarks=5, n_positions=5, d=3),
+        dict(n_landmarks=3, n_positions=3, d=2),
+    ]
+    remove_gauge_list = ["hard", "cost", None]
+
+    for param, remove_gauge in itertools.product(params, remove_gauge_list):
+        lifter = RangeOnlySLAM1Lifter(**param, remove_gauge=remove_gauge)
+
+        Q, y = lifter.get_Q(noise=0)
+        theta = lifter.get_vec_around_gt(delta=0).flatten("C")
+
+        grad = lifter.get_grad(theta, y)
+        hess = lifter.get_hess(theta, y)
+
+        np.testing.assert_almost_equal(grad, 0.0)
+
+        S, V = np.linalg.eig(hess)
+        mask = np.abs(S) < 1e-10
+        n_null = np.sum(mask)
+
+        if remove_gauge == "hard":
+            assert n_null == 0
+        else:
+            assert n_null > 0
+        continue
+
+        eigvecs = V[:, mask]
+        print(f"Hessian has nullspace of dimension {n_null}! Eigenvectors:", eigvecs)
+
+        fig, ax = lifter.plot_setup(title=f"Nullspace dim: {n_null}")
+        for i in range(n_null):
+            vec = eigvecs[:, i]  # p0_x, p0_y, ...
+            lifter.plot_nullvector(vec, ax, color=f"C{i}")
+
+            # theta_delta = theta + eigvec
+            # cost_delta = lifter.get_cost(theta_delta, y)
+            # assert cost_delta < 1e-10, cost_delta
+    print("done")
+    return None
+
+
+def compare_solvers():
+    kwargs = {"method": None}
+
+    compare_solvers = [
+        "Nelder-Mead",
+        "Powell",  # "CG",  CG takes forever.
+        "BFGS",
+        "Newton-CG",
+        "TNC",
+    ]
+    noise = 1e-3
+    for lifter in all_lifters:
+        np.random.seed(0)
+
+        # noisy setup
+        Q, y = lifter.get_Q(noise=noise)
+        if Q is None:
+            continue
+
+        # test that we stay at real solution when initializing at it
+        theta_gt = lifter.get_vec_around_gt(delta=0)
+
+        import time
+
+        for solver in compare_solvers:
+            kwargs["method"] = solver
+            t1 = time.time()
+            theta_hat, msg, cost_solver = lifter.local_solver(
+                theta_gt, y, solver_kwargs=kwargs
+            )
+            ttot = time.time() - t1
+            if theta_hat is None:
+                print(solver, "failed")
+            else:
+                error = np.linalg.norm(theta_hat - theta_gt)
+                print(
+                    f"{solver} finished in {ttot:.4f}s, final cost {cost_solver:.1e}, error {error:.1e}"
+                )
+
+
 if __name__ == "__main__":
     import sys
+    import warnings
 
-    import pytest
+    # import pytest
+    # print("testing")
+    # pytest.main([__file__, "-s"])
+    # print("all tests passed")
 
-    print("testing")
-    pytest.main([__file__, "-s"])
-    print("all tests passed")
-    sys.exit()
+    test_solvers()
+    test_solvers_noisy()
+
+    test_gauge()
+    test_grad_finite_diff()
+    test_hess_finite_diff()
 
     test_cost()
     test_cost_noisy()
 
     test_equivalent_lifters()
-    test_grad_finite_diff()
-    test_hess_finite_diff()
 
     test_levels()
 
-    test_solvers()
-    test_solvers_noisy()
-
     test_constraints()
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        compare_solvers()
+    # sys.exit()

lifters/range_only_lifters.py
@@ -15,6 +15,10 @@ from poly_matrix.least_squares_problem import LeastSquaresProblem
 REMOVE_GAUGE = "hard"
 # REMOVE_GAUGE = "cost"
 
+SOLVER_KWARGS = dict(
+    method="Nelder-Mead",  # appears to be the fastest.
+)
+
 
 class RangeOnlyLifter(StateLifter):
     """Lifters for range-only localization and SLAM problems.
@@ -59,8 +63,6 @@ class RangeOnlyLifter(StateLifter):
             # TODO(FD) add tests
             self.edges = edges
         super().__init__(theta_shape=(self.N, d), M=self.M)
-
-        np.random.seed(1)
         self.generate_random_landmarks()
         self.generate_random_positions()
 
@@ -100,7 +102,7 @@ class RangeOnlyLifter(StateLifter):
         if len(theta) > N:
             if self.remove_gauge == "hard":
                 # range-only SLAM
-                landmarks = np.empty((self.n_landmarks, self.d))
+                landmarks = np.zeros((self.n_landmarks, self.d))
                 landmarks[0, :] = 0.0
                 landmarks[1, 0] = theta[N]
                 landmarks[1, 1] = 0.0
@@ -113,7 +115,7 @@ class RangeOnlyLifter(StateLifter):
                     landmarks[2, 0] = theta[N + 1]
                     landmarks[2, 1] = theta[N + 2]
                     landmarks[3:, :] = theta[N + 3 :].reshape(
-                        (self.n_landmarks - 2, self.d)
+                        (self.n_landmarks - 3, self.d)
                     )
             else:
                 landmarks = theta[N:].reshape((-1, self.d))
@@ -130,14 +132,14 @@ class RangeOnlyLifter(StateLifter):
             )
             ** 2
         )
-        y = y_gt + np.random.normal(loc=0, scale=noise)
+        y = y_gt + np.random.normal(loc=0, scale=noise, size=y_gt.shape)
         Q = self.get_Q_from_y(y)
         return Q, y
 
     def get_J(self, t, y):
         J = np.empty((self.dim_x, self.N))
         J[0, :] = 0  # corresponds to homogenization variable
-        J[1 : self.N + 1, : self.N + 1] = np.eye(self.N)  # corresponds to theta
+        J[1 : self.N + 1, :] = np.eye(self.N)  # corresponds to theta
         J[self.N + 1 :, :] = self.get_J_lifting(t)  # corresponds to lifting
         return J
 
@@ -180,7 +182,9 @@ class RangeOnlyLifter(StateLifter):
             cost += (y[n, k] - y_current[n, k]) ** 2
         return cost
 
-    def local_solver(self, t_init, y, tol=1e-10, verbose=False):
+    def local_solver(
+        self, t_init, y, tol=1e-8, verbose=False, solver_kwargs=SOLVER_KWARGS
+    ):
         """
         :param t_init: (positions, landmarks) tuple
         """
@@ -191,9 +195,10 @@ class RangeOnlyLifter(StateLifter):
             x0=t_init,
             args=y,
             jac=self.get_grad,
-            method="Newton-CG",
+            # hess=self.get_hess, not used by any solvers.
+            **solver_kwargs,
             tol=tol,
-            options={"disp": verbose, "xtol": tol},
+            options={"disp": verbose},  # j, "maxfun": 100},
         )
         that = sol.x
         rel_error = self.get_cost(that, y) - self.get_cost(sol.x, y)
@@ -245,6 +250,52 @@ class RangeOnlyLifter(StateLifter):
     @property
     def var_dict(self):
         pass
+
+    def plot_setup(self, title="setup"):
+        import matplotlib.pylab as plt
+
+        if self.d == 3:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection="3d")
+        else:
+            fig, ax = plt.subplots()
+        ax.scatter(*self.landmarks.T, label="landmarks", marker="x")
+        ax.scatter(*self.positions.T, label="positions", marker="x")
+        ax.legend()
+        ax.axis("equal")
+        ax.grid()
+        ax.set_title(title)
+        plt.show()
+        return fig, ax
+
+    def plot_nullvector(self, vec, ax, **kwargs):
+        j = 0
+        for n in range(self.n_positions):
+            pos = self.positions[n]
+            line = np.c_[pos, pos + vec[j : j + self.d]]  # 2 x self.d
+            ax.plot(*line, **kwargs)
+            j += self.d
+        for n in range(self.n_landmarks):
+            if n == 1:
+                pos = self.landmarks[n]
+                e = np.zeros(self.d)
+                e[0] = vec[j]
+                line = np.c_[pos, pos + e]  # 2 x self.d
+                ax.plot(*line, **kwargs)
+                j += 1
+            elif n == 2:
+                pos = self.landmarks[n]
+                e = np.zeros(self.d)
+                e[:2] = vec[j : j + 2]
+                line = np.c_[pos, pos + e]  # 2 x self.d
+                ax.plot(*line, **kwargs)
+                j += 2
+            elif n > 2:
+                pos = self.landmarks[n]
+                e = vec[j : j + self.d]
+                line = np.c_[pos, pos + e]  # 2 x self.d
+                ax.plot(*line, **kwargs)
+                j += self.d
 
 
 class RangeOnlyLocLifter(RangeOnlyLifter):

lifters/range_only_slam1.py
@@ -1,3 +1,4 @@
+import matplotlib.pylab as plt
 import numpy as np
 
 from lifters.range_only_lifters import RangeOnlyLifter
@@ -10,8 +11,10 @@ class RangeOnlySLAM1Lifter(RangeOnlyLifter):
     Uses substitution tau_i=||t_i||^2, alpha_k=||a_k||^2, e_ik = a_k @ t_i
     """
 
-    def __init__(self, n_positions, n_landmarks, d, edges=None):
-        super().__init__(n_positions, n_landmarks, d, edges=edges)
+    def __init__(self, n_positions, n_landmarks, d, edges=None, remove_gauge="hard"):
+        super().__init__(
+            n_positions, n_landmarks, d, edges=edges, remove_gauge=remove_gauge
+        )
 
     @property
     def M(self):
@@ -41,7 +44,7 @@ class RangeOnlySLAM1Lifter(RangeOnlyLifter):
                 # a1_x, a2_x, a2_y, a3, a4, ...
                 var_dict.update({"a1": 1})
                 var_dict.update({"a2": 2})
-                var_dict.update({f"a{k}": 1 for k in range(3, self.n_landmarks)})
+                var_dict.update({f"a{k}": self.d for k in range(3, self.n_landmarks)})
         else:
             var_dict.update({f"a{k}": self.d for k in range(self.n_landmarks)})
         return var_dict
@@ -100,23 +103,25 @@ class RangeOnlySLAM1Lifter(RangeOnlyLifter):
     def sample_feasible(self):
         self.generate_random_positions()
 
-    def fill_depending_on_k(self, J_lifting, counter, k, vec):
+    def fill_depending_on_k(self, row, k, vec):
         """Because of Gauge freedom removal,
         the first columns of the landmark-based part of J
         are incomplete.
         """
-        if k == 1:
+        if k == 0:
+            return
+        elif k == 1:
             i = self.n_positions * self.d
-            J_lifting[counter, i] = vec[0]
+            row[i] = vec[0]
         elif k == 2:
             i = self.n_positions * self.d + 1
-            J_lifting[counter, i : i + 2] = vec[:2]
+            row[i : i + 2] = vec[:2]
         elif k == 3:
             i = self.n_positions * self.d + 3
-            J_lifting[counter, i : i + self.d] = vec
+            row[i : i + self.d] = vec
         else:
-            i = self.n_positions * self.d + 3 + (k - 4) * self.d
-            J_lifting[counter, i : i + self.d] = vec
+            i = self.n_positions * self.d + 3 + (k - 3) * self.d
+            row[i : i + self.d] = vec
 
     def get_J_lifting(self, t):
         positions, landmarks = self.get_positions_and_landmarks(t)
@@ -129,7 +134,7 @@ class RangeOnlySLAM1Lifter(RangeOnlyLifter):
             counter += 1
         for k in range(self.n_landmarks):
             if self.remove_gauge == "hard":
-                self.fill_depending_on_k(J_lifting, counter, k, 2 * landmarks[k, :])
+                self.fill_depending_on_k(J_lifting[counter, :], k, 2 * landmarks[k, :])
             else:
                 i = (self.n_positions + k) * self.d
                 J_lifting[counter, i : i + self.d] = 2 * landmarks[k]
@@ -138,39 +143,90 @@ class RangeOnlySLAM1Lifter(RangeOnlyLifter):
             i = n * self.d
             J_lifting[counter, i : i + self.d] = landmarks[k]
             if self.remove_gauge == "hard":
-                self.fill_depending_on_k(J_lifting, counter, k, positions[n])
+                self.fill_depending_on_k(J_lifting[counter, :], k, positions[n])
             else:
                 i = (self.n_positions + k) * self.d
                 J_lifting[counter, i : i + self.d] = positions[n]
             counter += 1
         return J_lifting
 
+    def fill_hessian_depending_on_k(self, hessian, k, fix_i=None, val=2.0):
+        if k == 0:  # and (fix_i is None):
+            return  # no Hessian!
+        # elif k == 0:
+        #    pass
+
+        elif k == 1:
+            i = self.n_positions * self.d
+            if fix_i is None:
+                hessian[i, i] = val  # a1_x
+            else:
+                hessian[fix_i[0], i] = val
+                hessian[i, fix_i[0]] = val
+        elif k == 2:
+            i = self.n_positions * self.d + 1
+            var_i = range(i, i + 2)
+            if fix_i is None:
+                hessian[var_i, var_i] = val  # a2_x, a2_y
+            else:
+                hessian[fix_i[:2], var_i] = val  # a2_x, a2_y
+                hessian[var_i, fix_i[:2]] = val  # a2_x, a2_y
+        elif k == 3:
+            i = self.n_positions * self.d + 3
+            var_i = range(i, i + self.d)
+            if fix_i is None:
+                hessian[var_i, var_i] = val  # a3_x, a3_y, a3_z
+            else:
+                hessian[fix_i, var_i] = val
+                hessian[var_i, fix_i] = val
+        else:
+            i = self.n_positions * self.d + 3 + (k - 3) * self.d
+            var_i = range(i, i + self.d)
+            if fix_i is None:
+                hessian[var_i, var_i] = val
+            else:
+                hessian[fix_i, var_i] = val
+                hessian[var_i, fix_i] = val
+
     def get_hess_lifting(self, t):
         """return list of the hessians of the M lifting functions."""
-        if self.remove_gauge == "hard":
-            raise NotImplementedError(
-                "get_hess_lifting without Gauge freedom is not implemented yet"
-            )
-
         hessians = []
         for j in range(self.M):
             hessian = np.zeros((self.N, self.N))
-            i = j * self.d
+
+            # Hessian of || tau_j ||^2:  2 * I
             if j < self.n_positions:
+                i = j * self.d
                 hessian[range(i, i + self.d), range(i, i + self.d)] = 2
+
+            # Hessian of || alpha_j ||^2:  2 * I
             elif j < self.n_landmarks + self.n_positions:
-                hessian[range(i, i + self.d), range(i, i + self.d)] = 2
+                if self.remove_gauge == "hard":
+                    k = j - self.n_positions
+                    self.fill_hessian_depending_on_k(hessian, k)
+                else:
+                    i = j * self.d
+                    hessian[i : i + self.d, i : i + self.d] = 2.0
+
+            # Hessian of alpha_j@tau_j:  tau_j or alpha_j
             else:
                 n, k = self.edges[j - self.n_landmarks - self.n_positions]
-                i = n * self.d
-                j = (self.n_positions + k) * self.d
-                hessian[range(i, i + self.d), range(j, j + self.d)] = 2
-                hessian[range(j, j + self.d), range(i, i + self.d)] = 2
+                if self.remove_gauge == "hard":
+                    i = n * self.d
+                    self.fill_hessian_depending_on_k(
+                        hessian, k, fix_i=range(i, i + self.d), val=1.0
+                    )
+                else:
+                    i = n * self.d
+                    j = (self.n_positions + k) * self.d
+                    hessian[range(i, i + self.d), range(j, j + self.d)] = 1
+                    hessian[range(j, j + self.d), range(i, i + self.d)] = 1
             hessians.append(hessian)
         return hessians
 
     def __repr__(self):
         return f"rangeonlyslam1-{self.d}d"
+
 
 if __name__ == "__main__":
     lifter = RangeOnlySLAM1Lifter(n_positions=3, n_landmarks=4, d=2)