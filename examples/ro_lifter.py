1124aac0a18d86314e1877dc868767dfd5147f69
Frederike Duembgen <frederike.duembgen@gmail.com>
2023-10-31 10:44:39 -0400

    Add more examples

--- examples/ro_lifter.py
+++ examples/ro_lifter.py
@@ -5,16 +5,20 @@ from lifters.range_only_lifters import RangeOnlyLocLifter as Lifter
 from utils.plotting_tools import savefig
 
 
-def get_problem(t_init=[0, 0], centered=False, level="no"):
+def get_problem(t_init, centered=False, level="no"):
     # Create lifter
     np.random.seed(0)
     n_landmarks = 4
     d = 2
-    lifter = Lifter(n_positions=1, n_landmarks=n_landmarks, d=d, level=level)
+    lifter = Lifter(
+        n_positions=t_init.shape[0], n_landmarks=n_landmarks, d=d, level=level
+    )
     lifter.landmarks = np.c_[
         np.ones(n_landmarks) + np.random.rand(4) * 0.5, np.arange(n_landmarks)
     ]
-    lifter.theta = np.array([0.3, 1.0])
+    lifter.theta = np.hstack(
+        [np.full(n_positions, 0.3)[:, None], np.linspace(1, 3, n_positions)[:, None]]
+    ).flatten()
 
     Q, y = lifter.get_Q()
 
@@ -39,20 +43,28 @@ def get_problem(t_init=[0, 0], centered=False, level="no"):
 def plot_problem(prob, lifter, fname=""):
     Q = prob["Q"]
 
-    xx, yy = np.meshgrid(np.arange(-1, 4, 0.1), np.arange(-1, 4, 0.1))
-    zz = np.empty(xx.shape)
-    for i in range(xx.shape[0]):
-        for j in range(yy.shape[1]):
-            x = lifter.get_x(theta=np.array([xx[i, j], yy[i, j]]))
-            zz[i, j] = x.T @ Q @ x
-
-    fig, ax = plt.subplots()
-    ax.pcolormesh(xx, yy, np.log10(zz))
-    ax.scatter(
-        lifter.landmarks[:, 0], lifter.landmarks[:, 1], color="white", marker="o"
+    theta_hat = prob["x_cand"][1 : 1 + lifter.theta.size].reshape(
+        lifter.n_positions, -1
     )
-    ax.scatter(lifter.theta[0], lifter.theta[1], color="white", marker="*")
-    ax.scatter(prob["x_cand"][1], prob["x_cand"][2], color="green", marker="*")
+    theta_gt = lifter.theta.reshape(lifter.n_positions, -1)
+    n_plots = min(3, lifter.n_positions)
+    xx, yy = np.meshgrid(np.arange(-1, 4, 0.1), np.arange(-1, 4, 0.1))
+    fig, ax = plt.subplots(1, n_plots, squeeze=False)
+    for n in range(n_plots):
+        zz = np.empty(xx.shape)
+        for i in range(xx.shape[0]):
+            for j in range(yy.shape[1]):
+                theta = theta_gt.copy()
+                theta[n, :] = xx[i, j], yy[i, j]
+                x = lifter.get_x(theta=theta)
+                zz[i, j] = x.T @ Q @ x
+
+        ax[0, n].pcolormesh(xx, yy, np.log10(zz))
+        ax[0, n].scatter(
+            lifter.landmarks[:, 0], lifter.landmarks[:, 1], color="white", marker="o"
+        )
+        ax[0, n].scatter(theta_gt[n, 0], theta_gt[n, 1], color="white", marker="*")
+        ax[0, n].scatter(theta_hat[n, 0], theta_hat[n, 1], color="green", marker="*")
     plt.show()
     if fname != "":
         savefig(fig, fname)
@@ -61,24 +73,36 @@ def plot_problem(prob, lifter, fname=""):
 if __name__ == "__main__":
     from problem_utils import save_test_problem
 
-    number = 10
-    level = "no"
+    # number = 10
+    # level = "no"
 
     # number = 11
     # level = "quad"
+
+    # n_positions = 3
+    # number = 12
+    # level = "quad"
+
+    n_positions = 10
+    number = 13
+    level = "quad"
 
     for centered in [False, True]:
         appendix = "c" if centered else ""
 
         prob, lifter = get_problem(
-            t_init=np.array([0, 0]), centered=centered, level=level
+            t_init=np.zeros((n_positions, 2)), centered=centered, level=level
         )
         fname = f"certifiable-tools/_test/test_prob_{number}G{appendix}.pkl"
         save_test_problem(**prob, fname=fname)
         plot_problem(prob, lifter, fname=fname.replace(".pkl", ".png"))
 
         prob, lifter = get_problem(
-            t_init=np.array([2, 0]), centered=centered, level=level
+            t_init=np.hstack(
+                [np.full(n_positions, 2)[:, None], np.zeros((n_positions, 1))]
+            ),
+            centered=centered,
+            level=level,
         )
         fname = f"certifiable-tools/_test/test_prob_{number}L{appendix}.pkl"
         save_test_problem(**prob, fname=fname)
